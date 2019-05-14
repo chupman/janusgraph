ec981ab6e1290d66ac5b5de7e9980e18b1800bb9
Chris Hupman <chupman@us.ibm.com>
2019-05-14 15:15:35 -0700

    verifying added test will fail without code changes.
    Signed-off-by: Chris Hupman <chupman@us.ibm.com>

--- janusgraph-core/src/main/java/org/janusgraph/diskstorage/indexing/IndexMutation.java
+++ janusgraph-core/src/main/java/org/janusgraph/diskstorage/indexing/IndexMutation.java
@@ -16,7 +16,6 @@ package org.janusgraph.diskstorage.indexing;
 
 import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
-import org.janusgraph.core.Cardinality;
 import org.janusgraph.diskstorage.EntryMetaData;
 import org.janusgraph.diskstorage.Mutation;
 
@@ -34,25 +33,20 @@ import java.util.List;
 
 public class IndexMutation extends Mutation<IndexEntry,IndexEntry> {
 
-    private final KeyInformation.StoreRetriever storeRetriever;
     private final boolean isNew;
     private boolean isDeleted;
 
-    public IndexMutation(KeyInformation.StoreRetriever storeRetriever,
-                         List<IndexEntry> additions, List<IndexEntry> deletions,
+    public IndexMutation(List<IndexEntry> additions, List<IndexEntry> deletions,
                          boolean isNew, boolean isDeleted) {
         super(additions, deletions);
         Preconditions.checkArgument(!(isNew && isDeleted),"Invalid status");
-        this.storeRetriever = storeRetriever;
         this.isNew = isNew;
         this.isDeleted = isDeleted;
     }
 
-    public IndexMutation(KeyInformation.StoreRetriever storeRetriever,
-                         boolean isNew, boolean isDeleted) {
+    public IndexMutation(boolean isNew, boolean isDeleted) {
         super();
         Preconditions.checkArgument(!(isNew && isDeleted),"Invalid status");
-        this.storeRetriever = storeRetriever;
         this.isNew = isNew;
         this.isDeleted = isDeleted;
     }
@@ -75,16 +69,11 @@ public class IndexMutation extends Mutation<IndexEntry,IndexEntry> {
         isDeleted=false;
     }
 
-    private boolean isCollection(String field) {
-        KeyInformation keyInformation = storeRetriever.get(field);
-        return keyInformation != null && keyInformation.getCardinality() != Cardinality.SINGLE;
-    }
-
-    private final Function<IndexEntry,String> ENTRY2FIELD_FCT = new Function<IndexEntry, String>() {
+    public static final Function<IndexEntry,String> ENTRY2FIELD_FCT = new Function<IndexEntry, String>() {
         @Nullable
         @Override
         public String apply(final IndexEntry indexEntry) {
-            return isCollection(indexEntry.field) ? indexEntry.field + "." + indexEntry.value : indexEntry.field;
+            return indexEntry.field;
         }
     };
 
